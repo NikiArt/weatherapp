681433d07004adee9298b7164ac11dce739a1268
Никита Бойко <genertyniki@gmail.com>
2019-01-24 09:35:44 +0000

    Homework 6

--- app/src/main/java/ru/test/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/test/weatherapp/MainActivity.kt
@@ -3,19 +3,27 @@ package ru.test.weatherapp
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SettingsFragment.OnFragmentInteractionListener, ContentFragment.OnFragmentInteractionListener {
     val settingsFragment = SettingsFragment()
     val contentFragment = ContentFragment()
+    private var currentFragment: Int = 1
 
     override fun onFragmentInteraction(currentFragment: Int) {
-        /* fragmentTransaction.remove(settingsFragment)
-         fragmentTransaction.commit()
-         fragmentTransaction.add(R.id.fragment, contentFragment)
-         fragmentTransaction.commit() */
+        changeFragment(currentFragment)
+    }
+
+    private fun changeFragment(currentFragment: Int) {
         var fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.content_main_frame, if (currentFragment == 1) contentFragment else settingsFragment)
+        if (currentFragment == 1) {
+            fragmentTransaction.replace(R.id.content_main_frame, contentFragment)
+            this.currentFragment = 2
+        } else {
+            fragmentTransaction.replace(R.id.content_main_frame, settingsFragment)
+            this.currentFragment = 1
+        }
         fragmentTransaction.commit()
     }
 
@@ -32,9 +40,14 @@ class MainActivity : AppCompatActivity(), SettingsFragment.OnFragmentInteraction
         var fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.content_main_frame, settingsFragment)
         fragmentTransaction.commit()
-
-
     }
 
-
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return if (keyCode == KeyEvent.KEYCODE_BACK && currentFragment == 2) {
+            changeFragment(currentFragment)
+            true
+        } else {
+            super.onKeyDown(keyCode, event)
+        }
+    }
 }
