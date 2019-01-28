690f90eee09502463a6ac5030dab3e1ba87d1bc2
NikiArt <genertyniki@gmail.com>
2019-01-28 16:35:29 +0300

    Добавил фрагмент и его запуск. Список пока пустой

--- app/src/main/java/ru/test/weatherapp/MyAdapter.kt
+++ app/src/main/java/ru/test/weatherapp/MyAdapter.kt
@@ -1,18 +1,14 @@
 package ru.test.weatherapp
 
-import android.os.Parcel
-import android.os.Parcelable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import kotlinx.android.synthetic.main.item.view.*
 
 
-class MyAdapter() : RecyclerView.Adapter<ViewHolder>(), Parcelable {
-
-
-    constructor(parcel: Parcel) : this() {
-    }
+class MyAdapter() : RecyclerView.Adapter<ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
@@ -24,28 +20,10 @@ class MyAdapter() : RecyclerView.Adapter<ViewHolder>(), Parcelable {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = Settings.instance().weatherHistory[position].temperature.toString()
+        holder.textView.text = "1 " + Settings.instance().weatherHistory[position].temperature.toString()
     }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MyAdapter> {
-        override fun createFromParcel(parcel: Parcel): MyAdapter {
-            return MyAdapter(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MyAdapter?> {
-            return arrayOfNulls(size)
-        }
-    }
-
 }
 
-class ViewHolder(var textView: TextView) : RecyclerView.ViewHolder(textView) {
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    var textView: TextView = view.textView
 }
