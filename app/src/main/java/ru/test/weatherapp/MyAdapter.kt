06fe8cfa7d70bb08a06924947612935531183086
NikiArt <genertyniki@gmail.com>
2019-01-29 12:53:16 +0300

    Попытки добавить список RecyclerView

--- app/src/main/java/ru/test/weatherapp/MyAdapter.kt
+++ app/src/main/java/ru/test/weatherapp/MyAdapter.kt
@@ -1,5 +1,6 @@
 package ru.test.weatherapp
 
+import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,21 +10,28 @@ import kotlinx.android.synthetic.main.item.view.*
 
 
 class MyAdapter() : RecyclerView.Adapter<ViewHolder>() {
+    private val cities = mutableListOf("Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург")
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
-        return ViewHolder(view as TextView)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return Settings.instance().weatherHistory.size
+        //return Settings.instance().weatherHistory.size
+        return cities.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = "1 " + Settings.instance().weatherHistory[position].temperature.toString()
+        holder.textView.text = cities[position]
+        val lp = holder.textView.layoutParams as ConstraintLayout.LayoutParams
+        holder.textView.layoutParams = lp
+
+        //holder.textView.text = "1 " + Settings.instance().weatherHistory[position].temperature.toString()
     }
 }
 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    var textView: TextView = view.textView
+    var textView: TextView = view.item_text_view
+    var constraintView = view.item_constraint_view
 }
