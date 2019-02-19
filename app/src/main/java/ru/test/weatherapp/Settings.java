dbdda564a6dcec198ce9fb91b7ac9b77bd689ab2
NikiArt <genertyniki@gmail.com>
2019-02-19 20:08:45 +0300

    + apiKey в константу

--- app/src/main/java/ru/test/weatherapp/Settings.java
+++ app/src/main/java/ru/test/weatherapp/Settings.java
@@ -9,7 +9,7 @@ public class Settings {
     private Boolean wetness = false;
     private Boolean windSpeed = false;
     private Boolean airPressure = false;
-    private String apiKey = "4fd10dd3f01a413cb3b94151191402";
+    private static String API_KEY = "4fd10dd3f01a413cb3b94151191402";
     private static List<WeatherValue> weatherHistory;
 
     public Settings() {
@@ -28,7 +28,7 @@ public class Settings {
     }
 
     public String getApiKey() {
-        return apiKey;
+        return API_KEY;
     }
 
     public void setCity(String city) {
